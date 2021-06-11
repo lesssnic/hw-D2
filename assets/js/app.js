035144a5ace8c2173f0ec8e2042514eff0be75aa
lesssnic <antoxxxa.ua@gmail.com>
2021-06-11 12:35:54 +0300

    expand-diapozons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,41 @@
 
 
+let bankIdBase =    [
+           {name: 'MasterCard', key1: 222100, key2: 272099},
+           {name: 'MasterCard', key1: 510000, key2: 559999},
+           {name: 'Maestro', key1: 501800, key2: 501899},
+           {name: 'Maestro', key1: 502000, key2: 502099},
+           {name: 'Maestro', key1: 503800, key2: 503899},
+           {name: 'Maestro', key1: 589300, key2: 589399},
+           {name: 'Maestro', key1: 630400, key2: 630499},
+           {name: 'Maestro', key1: 675900, key2: 675999},
+           {name: 'Maestro', key1: 676100, key2: 676399},
+           {name: 'Maestro UK', key1: 675900, key2: 675999},
+           {name: 'Maestro UK', key1: 676770, key2: 676779},
+           {name: 'Maestro UK', key1: 676774, key2: 676774},
+           {name: 'VISA Electron', key1: 402600, key2: 402699},
+           {name: 'VISA Electron', key1: 417500, key2: 417500},
+           {name: 'VISA Electron', key1: 450800, key2: 450899},
+           {name: 'VISA Electron', key1: 484400, key2: 484499},
+           {name: 'VISA Electron', key1: 491300, key2: 491399},
+           {name: 'VISA Electron', key1: 491700, key2: 491799},
+           {name: 'VISA', key1: 400000, key2: 499999},
+           {name: 'American Express', key1: 340000, key2: 349999},
+           {name: 'American Express', key1: 370000, key2: 379999},
+           {name: 'Discover Card', key1: 601100, key2: 601199},
+           {name: 'Discover Card', key1: 622126, key2: 622925},
+           {name: 'Discover Card', key1: 644000, key2: 659999},
+           {name: 'JCB', key1: 352800, key2: 358999},
+           {name: 'InstaPayment', key1: 637000, key2: 639999},
 
+           {name: 'Else', key1: 000000, key2: 999999}
+                    ];
 
 function cardsValidator(cardNumber = bankCreditCardNumber.value){
 
     cardNumber = cardNumber.replace(/\s/g, '').split('').filter((item) => Number.isInteger(+item));
 
-let bankId = cardNumber.join('').slice(0, 2);
+let bankId = cardNumber.join('').slice(0, 6);
 
     sumCardNumber = cardNumber.reverse()
                 .map(function(item, i){
@@ -20,39 +49,22 @@ let bankId = cardNumber.join('').slice(0, 2);
                 }).reduce((sum, item) => sum + item);
 
     if (sumCardNumber%10==0){
-        validData = 'Карта валидна  ' + bankIdDetect(bankId);
+       
         resultValidationCard.innerHTML = `<tr><th>${cardNumber.reverse().join('')}</th><th>Карта валидна</th><th>${bankIdDetect(bankId)}</th></tr>`;
     }else{ 
         
-        validData = 'В номере карты ошибка';
         resultValidationCard.innerHTML = `<tr><th>${cardNumber.reverse().join('')}</th><th>Карта НЕ валидна</th><th></th></tr>`;
     }
 };
 
 function bankIdDetect(id){
-    if (id >=51 && id <= 55 ) {
-        return 'MasterCard'
-    }
-    if (id >=40 && id <= 49 ) {
-        return 'VISA'
-    }
-    if (id >=56 && id <= 58 || id == 50 || id == 63 || id == 67) {
-        return 'Maestro'
-    }
-    if (id == 60 ) {
-        return 'Discover'
-    }
-    if (id == 34 || id == 37 ) {
-        return 'American Express'
-    }
-    if (id == 62 ) {
-        return 'China UnionPay'
-    }
-    if (id ==30 || id == 36 || id == 38 ) {
-        return 'Diners Club'
-    }else {
-        return 'Else'
-    }
     
+    for(i = 0; i < bankIdBase.length; i++){
+        if (bankIdBase[i].key1 <= +id && bankIdBase[i].key2 >= +id ){
+            return bankIdBase[i].name;
+            
+        };
+       
+    };
 };
 
